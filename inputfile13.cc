67553797e86787a219bba0bb0681fd534285a5cb
zela1234 <zelayaish@gmail.com>
2017-05-11 17:08:30 -0700

    Create inputfile13.cc

---
+++ inputfile13.cc
@@ -0,0 +1,38 @@
+LET a = 2
+LET b = 10
+LET c = 18
+LET d = 19
+LET e = 20
+LET f = 21
+LET g = 22
+LET h = 23
+LET i = 24
+LET j = 3
+LET k = 4
+LET l = 5
+LET m = 4
+LET n = 3
+LET o = 2
+LET p = 1
+LET q = 0
+LET r = 6
+LET s = 7
+LET t = 8
+LET u = 9
+LET v = 10
+LET w = 11
+LET x = 0
+LET y = 68
+LET z = 1
+1
+2
+3
+4
+5
+1 + 1
+3 % 2
+4 ^ 3
+2 % 3
+0 * 100
+100 - 200
+q ^ x
